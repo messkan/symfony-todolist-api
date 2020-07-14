b05fa97e6c7aa47953ece8fb415515dbaec6eb79
Skander <skanders@live.fr>
2020-07-14 13:25:29 +0100

    testing connection to mongodb

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,6 +3,8 @@
 
 namespace App\Controller;
 
+use App\Document\Task;
+use Doctrine\ODM\MongoDB\DocumentManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -21,5 +23,28 @@ class DefaultController extends  AbstractController
     public function index(Request $request){
 
         return  new JsonResponse(array('test' => "works"));
+    }
+
+    /**
+     * @return JsonResponse
+     * @Route("/addingTask", name="addingTask" , methods={"GET"})
+     */
+    public function addingTask(DocumentManager $dm){
+        try{
+            /**
+             * @var Task task
+             */
+            $task = new Task();
+
+            $task->setContent("the first document persisted");
+
+            $dm->persist($task);
+            $dm->flush();
+
+            return new JsonResponse(array("done" => "true"));
+        }catch (\Exception $err) {
+            return new JsonResponse($err);
+        }
+
     }
 }
