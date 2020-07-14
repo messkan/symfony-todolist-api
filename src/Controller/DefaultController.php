dfc7bae46fe9aa4477ec4f44488c9203cbdf9986
Skander <skanders@live.fr>
2020-07-14 15:18:58 +0100

    removing code in default controller

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -26,26 +26,4 @@ class DefaultController extends  AbstractController
         return  new JsonResponse(array('test' => "works"));
     }
 
-    /**
-     * @return JsonResponse
-     * @Route("/addingTask", name="addingTask" , methods={"GET" , "POST"})
-     */
-    public function addingTask(DocumentManager $dm, Request $request){
-        try{
-            /**
-             * @var Task task
-             */
-            $task = new Task();
-
-            $task->setContent("");
-
-            $dm->persist($task);
-            $dm->flush();
-
-       //  $body = json_decode($request->getContent(), true);
-         return new JsonResponse(array('ok' => 'ok'));
-        }catch (\Throwable $th){
-            return new JsonResponse($th->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
-        }
-    }
 }
