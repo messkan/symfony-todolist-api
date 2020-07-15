a7b3e97a104b959541b343cbc7dab465ca9ec318
Skander <skanders@live.fr>
2020-07-15 14:42:11 +0100

    removing unused calls

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -4,19 +4,13 @@
 namespace App\Controller;
 
 
-use App\Repository\TaskRepository;
 use App\Service\TaskService;
-use Doctrine\ODM\MongoDB\DocumentManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use App\Document\Task;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\Encoder\JsonEncoder;
-use Symfony\Component\Serializer\Encoder\XmlEncoder;
-use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
-use Symfony\Component\Serializer\Serializer;
+
 
 /**
  * Class TaskController
--- src/Service/TaskService.php
+++ src/Service/TaskService.php
@@ -10,7 +10,6 @@ use App\Document\Task;
 use App\Manager\TaskManager;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Encoder\XmlEncoder;
-use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer;
 class TaskService
